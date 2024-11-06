5cfc360bd7df4d77f3d0e930908298fc8f3533e6
Aixhua <167004871+auix15@users.noreply.github.com>
2024-11-06 19:12:29 +0000

    first change on index and layout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
@@ -11,30 +11,29 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-//create your first component
+//first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
+    //verificación de BACKEND_URL
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
         <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
+            <ScrollToTop>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/demo" element={<Demo />} />
+                    <Route path="/single/:theid" element={<Single />} />
+                    <Route path="*" element={<h1>Not found!</h1>} />
+                </Routes>
+                <Footer />
+            </ScrollToTop>
         </div>
     );
-}; //coment
+};
 
 export default injectContext(Layout);
