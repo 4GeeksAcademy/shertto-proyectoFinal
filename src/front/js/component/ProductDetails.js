66e68ca2fdfbfe283d30eef6f0528b1e33406ecf
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-11-13 01:08:17 +0000

    pagina de productos momentanea

---
+++ src/front/js/component/ProductDetails.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../styles/productdetails.css"; // Asegúrate de que el archivo de estilos esté importado
+
+const ProductDetails = ({ id, name, description, price, image, rating, category }) => {
+  return (
+    <div className="product-card">
+      <div className="product-image-container">
+        <img src={image} alt={name} className="product-image" />
+      </div>
+      <div className="product-info">
+        <h3 className="product-name">{name}</h3>
+        <p className="product-category">Categoría: <span>{category}</span></p>
+        <p className="product-description">{description}</p>
+        <div className="product-price">
+          <span>${price}</span>
+        </div>
+        <button className="btn-buy">Añadir al carrito</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
