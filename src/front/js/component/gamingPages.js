fd3bfbe919b51798ac63eea9dcf2ad676902a552
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-11-17 21:58:45 +0000

    Articulos listos

--- src/front/js/component/gamingPages.js
+++ src/front/js/component/gamingPages.js
@@ -9,16 +9,22 @@ const GamingPage = () => {
   useEffect(() => {
     // Simulación de productos específicos para auriculares y accesorios
     setProducts([
-      { id: 1, name: "Auriculares Bluetooth", description: "Auriculares inalámbricos de alta calidad con cancelación de ruido", price: 150, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731620156/159581-800-800_td2spj_mwodnm.png", category: "Auriculares" },
-      { id: 2, name: "Cargador Rápido", description: "Cargador rápido compatible con varios dispositivos", price: 50, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731620156/159581-800-800_td2spj_mwodnm.png", category: "Accesorios" },
-      { id: 3, name: "Cable USB-C", description: "Cable de carga rápida para dispositivos USB-C", price: 25, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731620156/159581-800-800_td2spj_mwodnm.png", category: "Accesorios" },
-      // Agrega más productos según sea necesario
+      { id: 1, name: "Productos Gaming", description: "Devoko Silla Gaming con Masajeador y LED, Silla de Oficina Ergonómica con Reposapiés, Sillas Gaming para Oficina y Juegos con Iluminación RGB（Rojo）", price:149.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873911/71d6N1gOzkL._AC_SL1500_-removebg-preview_e46tc8.png" },
+      { id: 2, name: "Productos Gaming", description: "Devoko Silla Gaming con Masajeador y LED, Silla de Oficina Ergonómica con Reposapiés, Sillas Gaming para Oficina y Juegos con Iluminación RGB（Azúl）", price:149.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873911/71YAEZ-lj0L._AC_SL1500_-removebg-preview_neojgw.png" },
+      { id: 3, name: "Productos Gaming", description: "Devoko Silla Gaming con Masajeador y LED, Silla de Oficina Ergonómica con Reposapiés, Sillas Gaming para Oficina y Juegos con Iluminación RGB（Negro）", price:149.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873911/71h3MKywDCL._AC_SL1500_-removebg-preview_qnzpth.png" },
+      { id: 4, name: "Productos Gaming", description: "NJSJ Altavoces PC, 2.0 Sonido Estéreo, Altavoz PC con Luces de Colores RGB, 3,5mm Jack, Control de Volumen, para PC de Escritorio, Ordenador Portátil, Móvil, Computadora(Rosa)", price: 25.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873910/71w8v6OMJKL._AC_SL1500_-removebg-preview_jt8rks.png"},
+      { id: 5, name: "Productos Gaming", description: "NJSJ Altavoces PC, 2.0 Sonido Estéreo, Altavoz PC con Luces de Colores RGB, 3,5mm Jack, Control de Volumen, para PC de Escritorio, Ordenador Portátil, Móvil, Computadora(Blanco)", price: 25.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873911/71gz1a4X3EL._AC_SL1500_-removebg-preview_keqm5e.png" },
+      { id: 6, name: "Productos Gaming", description: "NJSJ Altavoces PC, 2.0 Sonido Estéreo, Altavoz PC con Luces de Colores RGB, 3,5mm Jack, Control de Volumen, para PC de Escritorio, Ordenador Portátil, Móvil, Computadora(Negro)", price: 25.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873911/61v-QGd22mL._AC_SL1001_-removebg-preview_v9gywu.png" },
+      { id: 7, name: "Productos Gaming", description: "Juego de Teclado y ratón para Disposición QWERTY Español USB RGB, GT817 104 Juego de Teclas y ratón Retroiluminados con Cable USB para PC Windows", price: 21.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873911/71LKZ32h0-L._AC_SL1500_-removebg-preview_qlucza.png" },
+      { id: 8, name: "Productos Gaming", description: "MARSGAMING MCP124, Combo Gaming 3en1, Teclado Ergonómico H-Mech LED RGB, Ratón RGB 3200DPI Switches HUANO, Alfombrilla Nanotextil 360x260mm, Compatibilidad Multiplataforma, Layout Español, Blanco", price: 22.65, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873911/61HSj3zpDfL._AC_SL1500_-removebg-preview_meom6v.png" },
+      { id: 9, name: "Productos Gaming", description: "MARSGAMING MCP124, Combo Gaming 3en1, Teclado Ergonómico H-Mech LED RGB, Ratón RGB 3200DPI Switches HUANO, Alfombrilla Nanotextil 360x260mm, Compatibilidad Multiplataforma, Layout Español, Negro", price: 20.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731873911/71kcrLQTIxL._AC_SL1500_-removebg-preview_bvzdwi.png" },
+      { id: 10, name: "Productos Gaming", description: "Orzly Teclado y Raton Gaming - Combo Ratón y Teclado USB [RGB LED Retroiluminación], Cascos Gamer, Alfombrilla de Ratón - Para Usuarios PC, Xbox y PS4 [Hornet RX250]", price: 59.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731874164/71S-pQNQyIL._AC_SL1500_-removebg-preview_1_bpuyaj.png" },
     ]);
   }, []);
 
   return (
     <div className="product-page">
-      <h1 className="title">Auriculares y Accesorios</h1>
+      <h1 className="title">Productos Gaming</h1>
       <div className="product-list">
         {products.map((product) => (
           <EarphoneDetails key={product.id} {...product} />
--- src/front/js/component/pcPage.js
+++ src/front/js/component/pcPage.js
@@ -9,16 +9,22 @@ const PcPage = () => {
   useEffect(() => {
     // Simulación de productos específicos para auriculares y accesorios
     setProducts([
-      { id: 1, name: "Auriculares Bluetooth", description: "Auriculares inalámbricos de alta calidad con cancelación de ruido", price: 150, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731620156/159581-800-800_td2spj_mwodnm.png", category: "Auriculares" },
-      { id: 2, name: "Cargador Rápido", description: "Cargador rápido compatible con varios dispositivos", price: 50, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731620156/159581-800-800_td2spj_mwodnm.png", category: "Accesorios" },
-      { id: 3, name: "Cable USB-C", description: "Cable de carga rápida para dispositivos USB-C", price: 25, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731620156/159581-800-800_td2spj_mwodnm.png", category: "Accesorios" },
-      // Agrega más productos según sea necesario
+      { id: 1, name: "Accesorios para PC", description: "Uineer Ratón con Cable, 7200 dpi Ajustable y 6 Botones, diseño ergonómico, ratón óptico USB optimizado para PC portátil para Trabajar y, Negro Mate", price:11.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880453/51WmJULWbHL._AC_SL1500_-removebg-preview_k2oquf.png" },
+      { id: 2, name: "Accesorios para PC", description: "Uineer Ratón con Cable, Clic silencioso, 3200 dpi Ajustable y 6 Botones, diseño ergonómico, ratón óptico USB optimizado para PC portátil para Trabajar y Jugar(Gris)", price:11.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880452/618W2aULm6L._AC_SX425_-removebg-preview_hf5dkp.png" },
+      { id: 3, name: "Accesorios para PC", description: "INPHIC Raton inalámbrico, 2.4G Raton inalámbrico Recargable con Receptor USB, 3 Niveles Ajustables De dpi para Computadora PortáTil, Pc, Mac, Computadora, Macbook, Negro", price:14.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880452/51ikMaS08HL._AC_SL1500_-removebg-preview_ht3qgl.png" },
+      { id: 4, name: "Accesorios para PC", description: "INPHIC Ratón Inalámbrico Recargable, [Actualizado], Ratón Inalámbrico 2.4G Silencioso 1600 dpi con Receptor USB para Ordenador Portátil Ordenador Mac Tableta, Nivel de Batería Visible, Blanco", price: 14.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880453/41V_vCPK4IL._AC_SL1500_-removebg-preview_ponj9l.png"},
+      { id: 5, name: "Accesorios para PC", description: "ewanxin Webcam 1080P Full HD CMOS Cámara Web de Alta Micrófono Reductor de Ruido y Corrección de Automática,USB Plug and Play,Base Giratoria de 360°,para PC Computadora Portátil, Videollamadas Juegos", price: 22.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880452/71Ms7MN08KL._AC_SL1500_-removebg-preview_mztyjm.png" },
+      { id: 6, name: "Accesorios para PC", description: "TECKNET 1080P Full HD CMOS Cámara Web de Alta Micrófono Reductor de Ruido y Corrección de Automática,USB Plug and Play,Base Giratoria de 360°,para PC Computadora Portátil, Videollamadas Juegos", price: 22.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880452/51zTrJmCDOS._AC_SL1326_-removebg-preview_wrwugp.png" },
+      { id: 7, name: "Accesorios para PC", description: "TECKNET Alfombrilla Ratón con Reposamuñecas, Gaming Mouse Pad Ergonómica con Apoyo, Espuma Viscoelástica, Tacto Agradable & Impermeable, Alfombra Raton con Cojín, Base de Goma no Deslizante", price: 9.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880452/71tIA7Z3y0L._AC_SX425_-removebg-preview_a33iqr.png" },
+      { id: 8, name: "Accesorios para PC", description: "TECKNET Alfombrilla de Ratón Apoyo de Gel, Cojín Alfombrilla de Ratón (Base de Goma no Deslizante) - Superficie Texturizada Impermeable", price: 9.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880452/71pvrDJvc6L._AC_SL1500_-removebg-preview_ib6yxu.png" },
+      { id: 9, name: "Accesorios para PC", description: "Perixx PERIBOARD-213W Teclado de Tijera USB silencioso con Cable - Diseño Compacto con Teclado numérico - Blanco - QWERTY Español con Ñ", price: 25.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880452/71Ms7MN08KL._AC_SL1500_-removebg-preview_mztyjm.png" },
+      { id: 10, name: "Accesorios para PC", description: "Perixx PERIBOARD-213B Teclado de Tijera USB silencioso con Cable - Diseño Compacto con Teclado numérico - Negro - Qwert Español con Ñ", price: 25.99, image: "https://res.cloudinary.com/dese5jn5f/image/upload/v1731880452/611SSEvwD3L._AC_SL1500_-removebg-preview_jfz0ub.png" },
     ]);
   }, []);
 
   return (
     <div className="product-page">
-      <h1 className="title">Auriculares y Accesorios</h1>
+      <h1 className="title">Accesorios para PC</h1>
       <div className="product-list">
         {products.map((product) => (
           <EarphoneDetails key={product.id} {...product} />
