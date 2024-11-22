76915e9cac36d376b7c9b078e04fc91ea80698fd
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-11-22 13:31:03 +0000

    cambios

--- src/front/js/component/offcanvasdetails.js
+++ src/front/js/component/offcanvasdetails.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "../../styles/offcanvas.css"; // Asegúrate de agregar estilos para el off-canvas
+import "../../styles/offcanvas.css";
 
 const OffCanvasDetails = ({ product, onClose }) => {
   return (
     <div className="offcanvas-container">
-      <div className="offcanvas-overlay" onClick={onClose}></div> {/* Cierra al hacer clic fuera */}
+      <div className="offcanvas-overlay" onClick={onClose}></div>
       <div className="offcanvas-content">
         <button className="offcanvas-close" onClick={onClose}>
           ×
@@ -14,8 +14,9 @@ const OffCanvasDetails = ({ product, onClose }) => {
         <p>{product.description}</p>
         <div className="product-price">Precio: ${product.price}</div>
         <button className="btn-buy">Añadir a la cesta</button>
-         {/* Información adicional, solo visible aquí */}
-         {product.additionalInfo1 && product.additionalInfo2 && product.additionalInfo3 &&(
+
+        {/* Información adicional alineada a la izquierda */}
+        {product.additionalInfo1 && product.additionalInfo2 && product.additionalInfo3 && (
           <div className="product-additional-info">
             <h6>Acerca de este producto</h6>
             <p>• {product.additionalInfo1}</p>
@@ -23,7 +24,6 @@ const OffCanvasDetails = ({ product, onClose }) => {
             <p>• {product.additionalInfo3}</p>
           </div>
         )}
-        <button className="btn-buy">Añadir a la cesta</button>
       </div>
     </div>
   );
